4231abeb1ba018473c151f9467bf58b0cae3ff61
DanuxPeach <21520682@gm.uit.edu.vn>
2023-11-26 03:03:30 +0700

    update code and dockerfile

--- test.py
+++
@@ -1,2 +0,0 @@
-import psycopg2
-psycopg2.connect("dbname=user_database user=postgres host=localhost password=05112003 port=5432")
--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_bcrypt import Bcrypt
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = b'\x90\xe6-\x85\xdd\nfLH~_\xbc\x0b\xae\xe4\x0b\x86%E\xc4\x8b%T\xb4'  # Thay thế bằng một secret key bảo mật
 
 # Kết nối đến PostgreSQL
-conn = psycopg2.connect(user="postgresql", password="Dang0511!", host="postgresqlwebapp.postgres.database.azure.com", port=5432, database="user_database")
+#ConnectionString="postgresql://postgresql:Dang0511!@postgresqlwebapp.postgres.database.azure.com:5432/user_database"
+#conn = psycopg2.connect(user="postgresql", password="Dang0511!", host="postgresqlwebapp.postgres.database.azure.com", port=5432, database="user_database")
+connectString = os.getenv("ConnectionString")
+conn = psycopg2.connect(connectString)
 cur = conn.cursor()
 
 # Route cho trang đăng nhập
@@ -26,14 +33,19 @@ bcrypt = Bcrypt(app)
 def register_user():
     username = request.form['username']
     password = request.form['password']
-    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-    try:
-        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, hashed_password))
-        conn.commit()
-        return redirect('/')
-    except psycopg2.Error as e:
-        conn.rollback()
-        return f"Lỗi khi đăng ký: {e}"
+    confirm_password = request.form['confirm_password']
+    if password != confirm_password:
+        return redirect('/register?error=Mật khẩu và nhập lại mật khẩu không khớp. Vui lòng thử lại.')
+    else:
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        try:
+            cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, hashed_password))
+            conn.commit()
+            return redirect('/')
+        except psycopg2.Error as e:
+            conn.rollback()
+            error_message = f"Lỗi khi đăng ký: {e}"
+            return redirect(f'/register?error={error_message}')
 
 # Xử lý đăng nhập
 @app.route('/login', methods=['POST'])
@@ -66,4 +78,4 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, use_reloader=False)
